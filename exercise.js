aab426ef0a569a7b9ac9a5e26982ef2c9927aed7
Brayton Whitlock <brayton94@outlook.com>
2023-02-11 16:33:58 -0800

    19.1

---
+++ exercise.js
@@ -0,0 +1,20 @@
+// HTTP is "Hypertext Transfer Protocol".
+
+// URL is "Uniform Reasource Locator", it is an address for something on the internet.
+
+// DNS is "Domain Name System", this takes the URL that and converts them into IP adresses.
+
+// A query string is something that provides extra information, like search terms or information from forms.
+
+// GET- gets data from the server, requests without side effects.
+// POST- sends data to the server that was requested, request with side effects.
+
+// A HTTP request is a HTTP method(GET), that accesses a resource on the server.
+
+// A HTTP response is what is sent back after the request, it sends back JS, CSS, HTML files that are on the server.
+
+// An HTTP header is something that gives information about a request or response. 
+// Requests: content-type, retry-after, content-encoding.
+// Response: text/html; charset=utf-8, 46, br.
+
+// DNS turns the hostname into an IP address, the browser makes a request to that IP address, A response is sent back to the browser, the browser then "dynamically generates" a page, the resource provides any extra information like css or images.
